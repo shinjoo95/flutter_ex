be1c87385d6f462a42e934ed2262e86ab83e08db
shinjoo95 <shinjoo95@naver.com>
2022-10-27 16:31:44 +0900

    GET / POST

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -10,8 +12,27 @@ void main() {
 }
 var a = TextStyle();
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0;
+  var data = [];
+
+  getData() async{
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    data = result2;
+  }
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +46,65 @@ class MyApp extends StatelessWidget {
         ],
         actionsIconTheme: IconThemeData(color: Colors.black38, size: 30.0),
       ),
-      body: Container(),
+      body:
+        [Home(data : data), Text('샵')][tab],
+
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        onTap: (i){
+          setState(() {
+            tab = i;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             label: 'home',
             icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_outlined),
           ),
           BottomNavigationBarItem(
             label: 'shop',
             icon: Icon(Icons.shopping_bag_outlined),
-            activeIcon: Icon(Icons.shopping_bag),
             )
         ],
       ),
     );
   }
 }
+
+//home 페이지
+class Home extends StatelessWidget {
+  const Home({Key? key, this.data}) : super(key: key);
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    if(data != null){
+      return ListView.builder(itemCount: 3, itemBuilder: (c,i){
+        return Column(
+          children: [
+            Container(
+              constraints: BoxConstraints(maxWidth: 600),
+              padding: EdgeInsets.all(5),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(data[i]['image']),
+                  Text('좋아요${data[i]['likes']}'),
+                  Text(data[i]['user']),
+                  Text(data[i]['content']),
+                  Text(data[i]['date']),
+                ],
+              ),
+            )
+          ],
+
+        );
+      });
+    }else {
+      return Text('로딩중임');
+    }
+    }
+  }
+
