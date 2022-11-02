08c2ac30b0ff8ed623c158460e1953106a667229
shinjoo95 <shinjoo95@naver.com>
2022-11-02 19:52:11 +0900

    firebase 설치

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,17 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async{
+
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (c) => Store1()),
