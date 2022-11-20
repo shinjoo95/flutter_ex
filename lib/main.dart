06f962e9aa1ded576967a0cedd7a852f67127d76
shinjoo95 <shinjoo95@naver.com>
2022-11-20 16:54:51 +0900

    Intro 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'people.dart';
 import 'secondPage.dart';
+import 'intro.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AnimationApp(),
+      home: IntroPage(),
     );
   }
 }
